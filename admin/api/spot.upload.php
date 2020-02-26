9d4532fbefe2f50f1dfe463a13ec75db70631a14
dawoonkim <hec8897@gmail.com>
2020-02-27 00:16:37 +0900

    spot 업로드 로직

---
+++ admin/api/spot.upload.php
@@ -0,0 +1,50 @@
+<?php
+  include("inc/conn.php");
+  mysqli_set_charset($conn,"utf-8"); 
+  header('Access-Control-Allow-Origin: *');  
+  $data = json_decode(file_get_contents("php://input"),true);
+  $reqTit = $_POST['reqtit'];
+  $reqStandard = $_POST['reqStandard'];
+  $reqDate = $_POST['reqDate'];
+  $Files = $_FILES['reqFile'];
+
+  function FileUploader($files){
+    $file = $files;
+    $upload_directory = '../../spot/';
+    $time = date('YmdHis');
+
+    $ext_str = "jpg,gif,png,JPG,GIF,PNG";
+    $allowed_extensions = explode(',', $ext_str);
+    $max_file_size = 3000000;
+    $ext = substr($file['name'], strrpos($file['name'], '.') + 1);
+    $path = $file['name'];
+    if($files == null){
+        $Results = 'fileNo';
+    }
+    else if(!in_array($ext, $allowed_extensions)) {
+        $Results = 'noExt';
+    }
+    else if($file['size'] >= $max_file_size){
+        $Results = 'huge';
+    }
+    else if(move_uploaded_file($file['tmp_name'], $upload_directory.$time."main_img.".$ext)) {
+            $Results = $time."main_img.".$ext;
+    }
+    return $Result = $Results;
+    }
+
+        $spotImg = FileUploader($Files);
+
+        $sql = "INSERT INTO `tb_spot` (`standard`, `reqtit`, `reqdate`, `imgroute`) VALUES ('$reqStandard', '$reqTit', '$reqDate', '$spotImg')";
+        $query = mysqli_query($conn,$sql);
+
+        $phpResult = isset($query)?"ok":"no";
+        $json =  json_encode(
+          array(
+              "phpResult"=> $phpResult
+          ));
+
+        echo urldecode($json);
+        @Header('Content-Type: application/json');
+        @Header('Content-Type: text/html; charset=utf-8');
+?>
--- admin/dist/bundle.js
+++ admin/dist/bundle.js
@@ -13408,16 +13408,24 @@ const etcModal = {
                         <ul class='inputs' v-else-if="this.mode === 'spotImg'">
                             <form>
                             <li>
-                                <input type='text' placeholder='현장사진 제목'>
+                                <input type='text' class='file_input' placeholder='현장사진 제목' id='reqtit'>
                             </li>
                             <li>
-                                <input type='text' placeholder='시공일자 (ex 2020.01.01)'>
+                                <input type='text' class='file_input' placeholder='시공일자 (ex 2020.01.01)' id='reqdate'>
+                            </li>
+                            <li>
+                                <select id='reqstandard'>
+                                    <option value="" disabled selected>분류</option>
+                                    <option value='삼성'>삼성</option>
+                                    <option value='천안'>천안,아산</option>
+                                    <option value='부동산'>부동산</option>
+                                </select>
                             </li>
                             <li>
                             <div class="input-file">
                             <label for="upload1" class="file-label">파일 선택</label> 
                             <input type="text" id='files_popup' readonly="readonly" class="file-name" placeholder='현장사진(1MB이내)'/> 
-                            <input type="file" id="upload1" class="file-upload" ref="mainimg" @change='fileChange($event.target.value)'/>
+                            <input type="file" id="upload1" class="file-upload" ref="filed_photo" @change='fileChange($event.target.value)'/>
                              </div>
                             </li>
                          
@@ -13427,8 +13435,8 @@ const etcModal = {
 
                     <div class="modal_foot">
                         <span class="b_blue" @click='PostData' v-if="mode === 'chpw'">확인</span>
-                        <span class="b_blue" @click=' shareData' v-else-if="mode === 'cflag'">확인</span>
-                        <span class="b_blue" v-else-if="mode === 'spotImg'">확인</span>
+                        <span class="b_blue" @click='shareData' v-else-if="mode === 'cflag'">확인</span>
+                        <span class="b_blue" @click='uploadSpot' v-else-if="mode === 'spotImg'">확인</span>
                         <span v-on:click='ModalClose' class="b_sgrey">취소</span>
                     </div>
                 </div>
@@ -13440,7 +13448,8 @@ const etcModal = {
             mode: null,
             lists: Array,
             SelectDatas: null,
-            ment: String
+            ment: String,
+            refFile:null
         }
     },
     created() {
@@ -13451,26 +13460,27 @@ const etcModal = {
             this.mode = 'chpw';
             this.ment = '패스워드 변경'
         })
-
         _eventbus_js__WEBPACK_IMPORTED_MODULE_0__["default"].$on('shareCflag', (Data) => {
             this.mode = 'cflag';
             this.Data = Data;
             this.ment = '상담사 배정'
         })
-
         _eventbus_js__WEBPACK_IMPORTED_MODULE_0__["default"].$on('spotImg', (Data) => {
             this.mode = 'spotImg';
             this.Data = Data;
             this.ment = '현장사진 등록'
         })
-
     },
-    mounted() {},
     methods: {
         ModalClose() {
+            const inputs = document.querySelectorAll('input')
+            for(let i = 0; i<inputs.length; i++){
+                inputs[i].value=''
+            }
+            this.refFile = null;
+
             const Modal = document.getElementById('modal-etc')
             Modal.style.opacity = '0';
-
             setTimeout(() => {
                 Modal.style.display = 'none';
             }, 100);
@@ -13516,8 +13526,18 @@ const etcModal = {
 
         },
         fileChange(thisValue){
-            const filesPopup = document.getElementById('files_popup')
-            filesPopup.value = thisValue;
+            const File =this.$refs.filed_photo.files[0] 
+            if(File.size > 2000000){
+                alert('파일사이즈가 너무 큽니다.')
+            }
+            else if(File.type !='image/png' && File.type !='image/jpeg'){
+                alert('지원하지 않는파일형식입니다.')
+            }
+            else{
+                const filesPopup = document.getElementById('files_popup')
+                filesPopup.value = thisValue;
+                this.refFile = File
+            }
         },
         PostData() {
             const reqPassword = document.getElementById('reqpassword')
@@ -13550,7 +13570,51 @@ const etcModal = {
                 alert('패스워드는 9자이상 입니다')
             }
         },
+        uploadSpot(){
+            const reqTit = document.getElementById('reqtit')
+            const reqDate = document.getElementById('reqdate')
+            const reqStandard = document.getElementById('reqstandard')
 
+            const File = this.refFile;
+
+            if(File == null){
+                alert('업로드할 파일은 선택해주세요')                
+            }
+            else if(reqTit.value == ""){
+                alert('제목을 입력해주세요')    
+                reqTit.focus()            
+            }
+            else if(reqDate.value == ""){
+                alert('시공날짜를 선택해주세요')                
+                reqDate.focus()
+            }
+            else if(reqStandard.value == ""){
+                alert('분류를 선택해주세요')                
+                reqStandard.focus()
+            }
+            else{
+                const baseURI = 'api/spot.upload.php';
+
+                let InsertData = new FormData();
+
+                InsertData.append('reqtit',reqTit.value)
+                InsertData.append('reqDate',reqDate.value)
+                InsertData.append('reqStandard',reqStandard.value)
+                InsertData.append('reqFile',File)
+
+
+                axios.post(`${baseURI}`, 
+                    InsertData
+                    )
+                    .then((result) => {
+                        if(result.data.phpResult == 'ok'){
+                            alert('등록되었습니다')
+                            this.ModalClose()
+                        }
+                    })
+                    .catch(err => console.log('Login: ', err));
+            }
+        }
     }
 }
 
